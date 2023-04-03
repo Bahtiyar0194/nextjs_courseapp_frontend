ab33cf78e339d11271e5e534dd3339d9b06fe44e
Baktiyar <bahtiyar_0194@mail.ru>
2023-04-03 17:10:49 +0500

    thirthy two commit

--- config/api.js
+++ config/api.js
@@ -1,3 +1,3 @@
-const API_URL = process.env.NODE_ENV === 'development' ? 'http://lms-platform.loc/api/v1' : 'http://shaqyry.kz/api/v1';
+const API_URL = process.env.NODE_ENV === 'development' ? 'http://lms-platform.loc/api/v1' : 'https://shaqyry.kz/api/v1';
 
 export default API_URL;
