8c510951a6d077f820e77f18b1a8d0791a6f4b55
Baktiyar <bahtiyar_0194@mail.ru>
2023-04-04 11:13:12 +0500

    thirthy four commit

--- config/main_domain.js
+++ config/main_domain.js
@@ -1,3 +1,3 @@
-const MAIN_DOMAIN = process.env.NODE_ENV === 'development' ? 'localhost:3000' : window.location.host;
+const MAIN_DOMAIN = process.env.NODE_ENV === 'development' ? 'localhost:3000' : 'nextjs-courseapp-frontend-git-main-bahtiyar0194.vercel.app';
 
 export default MAIN_DOMAIN;
