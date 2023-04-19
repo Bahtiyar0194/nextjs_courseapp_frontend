b9211d7e5b8a3f5f0415f0f5280bc2dde6df566c
Baktiyar <bahtiyar_0194@mail.ru>
2023-04-19 16:54:05 +0500

    thirty seven commit

--- components/main-page/FadeSlider.js
+++ components/main-page/FadeSlider.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Fade } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css';
-import IPhoneScreen from '../misc/IPhoneScreen';
+import IphoneScreen from '../misc/IphoneScreen';
 import MacBookScreen from '../misc/MacBookScreen';
 
 const fadeImages = [
@@ -51,9 +51,9 @@ const FadeSlider = () => {
 
             <div className='custom-grid absolute bottom-0 z-20'>
                 <div className='col-span-5 col-start-11 md:col-span-6 md:col-start-11 lg:col-span-7 lg:col-start-11'>
-                    <IPhoneScreen>
+                    <IphoneScreen>
                         <img src='/img/index/users-mobile.png'/>
-                    </IPhoneScreen>
+                    </IphoneScreen>
                 </div>
             </div>
         </div>
